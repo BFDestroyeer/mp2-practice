31332185a14b5e35351d99c43c7ae7cea7283532
MIkhail-Zoreev <mzoreev@gmail.com>
2019-10-04 21:32:43 +0300

    Fix no3

--- src/01_Matrix/01_Matrix/Main.cpp
+++ src/01_Matrix/01_Matrix/Main.cpp
@@ -9,36 +9,38 @@
 
 int main()
 {
-    unsigned size;
+    unsigned sizeA, sizeB, sizeD;
 
     setlocale(LC_ALL, "Russian");
 
     std::cout << "Программа провекрки шаблона верхней треугольной матрицы" << std::endl;
     std::cout << "2019 год. Все права защищены" << std::endl;
 
-    std::cout << std::endl << "Введите размер матриц ";
-    std::cin >> size;
+    std::cout << std::endl << "Введите размер матрицы A ";
+    std::cin >> sizeA;
+    std::cout << "Введите размер матрицы B ";
+    std::cin >> sizeB;
+    std::cout << "Введите размер вектора D ";
+    std::cin >> sizeD;
 
-    TMatrix<double> a(size), b(size);
+    TMatrix<double> a(sizeA), b(sizeB);
     double c;
-    TVector<double> d(size);
+    TVector<double> d(sizeD);
 
-    std::cout << "Введите матриицу A, размера " << size <<std::endl;
+    std::cout << "Введите матриицу A, размера " << sizeA <<std::endl;
     std::cin >> a;
-    std::cout << "Введите матриицу B, размера " << size <<std::endl;
+    std::cout << "Введите матриицу B, размера " << sizeB <<std::endl;
     std::cin >> b;
     std::cout << "Введите константу c" << std::endl;
     std::cin >> c;
-    std::cout << "Введите вектор d, размера " << size << std::endl;
+    std::cout << "Введите вектор d, размера " << sizeD << std::endl;
     std::cin >> d;
 
     std::cout << std::endl << "Проверка конструкторов" << std::endl;
-    /*std::cout << "Конструктор с параметрами" << std::endl;
-    std::cout << TMatrix<double>(size) << std::endl;*/
     std::cout << "Конструктор копирования" << std::endl;
     std::cout << TMatrix<double>(a) << std::endl;
     std::cout << "Конструктор преобразования типа" << std::endl;
-    std::cout << TMatrix<double>(Generate::VectorOfVectors(size)) << std::endl;
+    std::cout << TMatrix<double>(Generate::VectorOfVectors(sizeA)) << std::endl;
 
     std::cout << "Операции сравнения" << std::endl;
     std::cout << "A == B " << (a == b) << std::endl;
@@ -54,15 +56,43 @@ int main()
 
     std::cout << "Матричные операции" << std::endl;
     std::cout << "A+B" << std::endl;
-    std::cout << (a + b) << std::endl;
+    try
+    {
+        std::cout << (a + b) << std::endl;
+    }
+    catch (TException)
+    {
+        std::cout << "BadSize брошен успешно" << std::endl;
+    }
     std::cout << "A-B" << std::endl;
-    std::cout << (a - b) << std::endl;
+    try
+    {
+        std::cout << (a - b) << std::endl;
+    }
+    catch (TException)
+    {
+        std::cout << "BadSize брошен успешно" << std::endl;
+    }
     std::cout << "A*B" << std::endl;
-    std::cout << (a * b) << std::endl;
+    try
+    {
+        std::cout << (a * b) << std::endl;
+    }
+    catch (TException)
+    {
+        std::cout << "BadSize брошен успешно" << std::endl;
+    }
 
     std::cout << "Операция умножения на вектор" << std::endl;
     std::cout << "A*d" << std::endl;
-    std::cout << (a * d) << std::endl;
+    try
+    {
+        std::cout << (a * d) << std::endl;
+    }
+    catch (TException)
+    {
+        std::cout << "BadSize брошен успешно" << std::endl;
+    }
 
     std::cout << std::endl << "Операция присваивания" << std::endl;
     std::cout << (TMatrix<double>() = a) << std::endl;
@@ -70,29 +100,32 @@ int main()
     std::cout << "Нахождение определителя" << std::endl;
     std::cout << a.Determinant() << std::endl;
 
-    std::cout << std::endl << "Проверка исключений" << std::endl;
+    std::cout << std::endl << "Проверка особых исключений" << std::endl;
     try
     {
-        a * TMatrix<double>(5);
+        std::cout << "Обращаюсь к недоступному полю" << std::endl;
+        a[a.GetSize() + 1];
     }
     catch (TException)
     {
-        std::cout << "BadSize брошен успешно" << std::endl;
+        std::cout << "BadIndex брошен успешно" << std::endl << std::endl;
     }
     try
     {
-        a[10];
+        std::cout << "Генерирую вектор нулевого размера" << std::endl;
+        TVector<double> temp(0);
     }
     catch (TException)
     {
-        std::cout << "BadIndex брошен успешно" << std::endl;
+        std::cout << "Can't create empty брошен успешно" << std::endl << std::endl;
     }
     try
     {
-        a[3][0] = 1;
+        std::cout << "Преобразую неподходящую матрицу" << std::endl;
+        TMatrix<double> temp(TVector<TVector<double> >(3));
     }
     catch (TException)
     {
-        std::cout << "WriteOnReadOnly брошен успешно" << std::endl;
+        std::cout << "Can't convert брошен успешно" << std::endl;
     }
 }
--- src/01_Matrix/01_Matrix/TExcepntion.h
+++ src/01_Matrix/01_Matrix/TExcepntion.h
@@ -4,7 +4,6 @@ enum Exception_Type
 {
     BadSize,        //Неправльный размер матрицы или вектора
     BadIndex,       //Неправильный индекс
-    WriteOnReadOnly, //Попытка записи в ячейку доступную только для чтения
     CantCreateEmpty,
     CantConvert
 };
