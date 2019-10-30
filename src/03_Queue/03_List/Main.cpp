c09dc7b8bdeeb324a130aff592b4ad2bcf1659e2
MIkhail-Zoreev <mzoreev@gmail.com>
2019-10-30 09:50:33 +0300

    AAAAAAAAAAAv2

--- src/03_Queue/03_List/Main.cpp
+++ src/03_Queue/03_List/Main.cpp
@@ -5,6 +5,9 @@ int main()
 	TList<int, int> test;
 	int* a = new int;
 	*a = 100;
-	test.InstertForward(10, a);
+	test.InsertBackward(20, a);
+	test.InsertBackward(10, a);
+	test.InsertAfter(10, 5, a);
+	test.Remove(5);
 	return 0;
 }
--- src/03_Queue/03_List/TList.h
+++ src/03_Queue/03_List/TList.h
@@ -19,10 +19,10 @@ public:
 	TList(const TNode<TKey, TData>* node_);
 	~TList();
 
-	void InstertForward(TKey key_, TData* pData_);
-	void InstertBackward(TKey key_, TData* pData_);
-	void InstertBefore(TKey key_a, TKey key_, TData* pData_);
-	void InstertAfter(TKey key_b ,TKey key_, TData* pData_);
+	void InsertForward(TKey key_, TData* pData_);
+	void InsertBackward(TKey key_, TData* pData_);
+	void InsertBefore(TKey key_b, TKey key_, TData* pData_);
+	void InsertAfter(TKey key_a ,TKey key_, TData* pData_);
 	
 	void Remove(TKey key_);
 
@@ -34,10 +34,10 @@ public:
 template <typename TKey, typename TData>
 TList<TKey, TData>::TList()
 {
-	pFirst = new TNode<TKey, TData>();
+	pFirst = nullptr;
 	pPrevious = nullptr;
 	pNext = nullptr;
-	pCurrent = pFirst;
+	pCurrent = nullptr;
 }
 
 template <typename TKey, typename TData>
@@ -76,10 +76,99 @@ TList<TKey, TData>::~TList()
 }
 
 template <typename TKey, typename TData>
-void TList<TKey, TData>::InstertForward(TKey key_, TData* pData_)
+void TList<TKey, TData>::InsertForward(TKey key_, TData* pData_)
 {
+	if (pFirst == nullptr)
+	{
+		pFirst = new TNode<TKey, TData>(key_, pData_);
+		pCurrent = pFirst;
+		return;
+	}
 	TNode<TKey, TData>* temp = new TNode<TKey, TData>(key_, pData_);
 	temp->pNext = pFirst;
 	if (pCurrent == pFirst) pPrevious = temp;
 	pFirst = temp;
+}
+
+template <typename TKey, typename TData>
+void TList<TKey, TData>::InsertBackward(TKey key_, TData* pData_)
+{
+	if (pFirst == nullptr)
+	{
+		pFirst = new TNode<TKey, TData>(key_, pData_);
+		pCurrent = pFirst;
+		return;
+	}
+	TNode<TKey, TData>* temp = new TNode<TKey, TData>(key_, pData_);
+	TNode<TKey, TData>* node = pFirst;
+	while (node->pNext != nullptr)
+	{
+		node = node->pNext;
+	}
+	node->pNext = temp;
+	if (pCurrent = node) pNext = temp;
+}
+
+template <typename TKey, typename TData>
+void TList<TKey, TData>::InsertBefore(TKey key_b, TKey key_, TData* pData_)
+{
+	if (pFirst == nullptr) return;
+	if (pFirst->key == key_b)
+	{
+		InsertForward(key_, pData_);
+		return;
+	}
+	TNode<TKey, TData>* prev_node = pFirst;
+	while ((prev_node->pNext != nullptr) && (prev_node->pNext->key != key_b))
+	{
+		prev_node = prev_node->pNext;
+	}
+	if (prev_node->pNext == nullptr) throw "Can't find";
+	TNode<TKey, TData>* next_node = prev_node->pNext;
+	TNode<TKey, TData>* temp = new TNode<TKey, TData>(key_, pData_);
+	prev_node->pNext = temp;
+	temp->pNext = next_node;
+	if (pCurrent == prev_node) pNext = temp;
+	if (pCurrent == next_node) pPrevious = temp;
+}
+
+template <typename TKey, typename TData>
+void TList<TKey, TData>::InsertAfter(TKey key_a, TKey key_, TData* pData_)
+{
+	if (pFirst == nullptr) return;
+	TNode<TKey, TData>* prev_node = pFirst;
+	while ((prev_node != nullptr) && (prev_node->key != key_a))
+	{
+		prev_node = prev_node->pNext;
+	}
+	if (prev_node == nullptr) throw "Can't find";
+	TNode<TKey, TData>* next_node = prev_node->pNext;
+	TNode<TKey, TData>* temp = new TNode<TKey, TData>(key_, pData_);
+	prev_node->pNext = temp;
+	temp->pNext = next_node;
+	if (pCurrent == prev_node) pNext = temp;
+	if (pCurrent == next_node) pPrevious = temp;
+
+}
+
+template <typename TKey, typename TData> //Поправить итератор
+void TList<TKey, TData>::Remove(TKey key_)
+{
+	if (pFirst == nullptr) return;
+	if (pFirst->key == key_)
+	{
+		TNode<TKey, TData>* next_node = pFirst->pNext;
+		delete pFirst;
+		pFirst = next_node;
+		return;
+	}
+	TNode<TKey, TData>* prev_node = pFirst;
+	while ((prev_node->pNext != nullptr) && (prev_node->pNext->key != key_))
+	{
+		prev_node = prev_node->pNext;
+	}
+	if (prev_node->pNext == nullptr) throw "Can't find";
+	TNode<TKey, TData>* next_node = prev_node->pNext->pNext;
+	delete prev_node->pNext;
+	prev_node->pNext = next_node;
 }
