44b5e91cbf39f710907b1cdcf35f6cfcb5129a66
MIkhail-Zoreev <mzoreev@gmail.com>
2019-11-22 12:16:23 +0300

    up

--- src/03_List/03_List/Main.cpp
+++ src/03_List/03_List/Main.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	TPolynom test;
 	TNode<int, double> node(123, 10), tnode(124, 5);
-	test = test + node;
-	test = test + node;
-	std::cout << test;
+	test = test + node + tnode;
+	TPolynom a(test);
+	std::cout << test * node;
 }
--- src/03_List/03_List/TPolynom.h
+++ src/03_List/03_List/TPolynom.h
@@ -16,7 +16,9 @@ public:
 	TPolynom operator+(const TPolynom& temp);
 	TPolynom operator+(const TNode<int, double>& node);
 	TPolynom operator-(const TPolynom& temp);
+	TPolynom operator-(const TNode<int, double>& node);
 	TPolynom operator*(const TPolynom& temp);
+	TPolynom operator*(const TNode<int, double>& node);
 
 	friend std::ostream& operator<<(std::ostream& out, const TPolynom& polynom);
 };
@@ -46,6 +48,12 @@ TPolynom& TPolynom::operator=(const TPolynom& temp)
 TPolynom TPolynom::operator+(const TPolynom& temp)
 {
 	TPolynom out(*this);
+	TNode<int, double>* iter = temp.list->pFirst;
+	while (iter != nullptr)
+	{
+		out = out + *iter;
+		iter = iter->pNext;
+	}
 	return out;
 }
 
@@ -68,6 +76,65 @@ TPolynom TPolynom::operator+(const TNode<int, double>& node)
 		iter = iter->pNext;
 	}
 	out.list->InsertBackward(node.key, node.pData);
+	return out;
+}
+
+TPolynom TPolynom::operator-(const TPolynom& temp)
+{
+	TPolynom out(*this);
+	TNode<int, double>* iter = temp.list->pFirst;
+	while (iter != nullptr)
+	{
+		out = out - *iter;
+		iter = iter->pNext;
+	}
+	return out;
+}
+
+TPolynom TPolynom::operator-(const TNode<int, double>& node)
+{
+	TPolynom out(*this);
+	TNode<int, double>* iter = out.list->pFirst;
+	while (iter != nullptr)
+	{
+		if (iter->key == node.key)
+		{
+			*iter = *iter - node;
+			return out;
+		}
+		if (iter->key < node.key)
+		{
+			out.list->InsertBefore(iter->key, node.key, -*node.pData);
+			return out;
+		}
+		iter = iter->pNext;
+	}
+	out.list->InsertBackward(node.key, -*node.pData);
+	return out;
+}
+
+TPolynom TPolynom::operator*(const TPolynom& temp)
+{
+	TPolynom out(*this);
+	TNode<int, double>* iter = temp.list->pFirst;
+	while (iter != nullptr)
+	{
+		out = out * *iter;
+		iter = iter->pNext;
+	}
+	return out;
+	return out;
+}
+
+TPolynom TPolynom::operator*(const TNode<int, double>& node)
+{
+	TPolynom out(*this);
+	TNode<int, double>* iter = out.list->pFirst;
+	while (iter != nullptr)
+	{
+		*iter = *iter * node;
+		iter = iter->pNext;
+	}
 	return out;
 }
 
